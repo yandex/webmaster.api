2956943569ade2c6a68bf4cda46ab341e89d9362
dvpov <dima@subdomain.ru>
2016-09-08 14:38:00 +0300

    add readme file

--- example/conf.php
+++ example/conf.php
@@ -1,10 +1,9 @@
 <?php
 
-
 $path = realpath(dirname(__FILE__));
 $apiScript = realpath($path."/../webmaster_api.class.php");
 
-
+// Get this code on https://oauth.yandex.com/client/new page
 $client_id = '';
 $client_secret = '';
 
