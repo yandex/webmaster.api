d36afc9bd21103afbff14368c07dcdd85d06bbb5
Dmitriy Popov <dima@subdomain.ru>
2016-09-11 17:55:24 +0300

    Sequrity error fix

--- example/auth.php
+++ example/auth.php
@@ -7,19 +7,24 @@ if(empty($client_id)||empty($client_secret))
     die('Please, enter your client id and secret code to conf.php file');
 }
 
+// Check access token
 if(!isset($_SESSION['access_token']))
 {
-    if(!isset($_GET['code']))
+    // if there we have callback param from ouath server try get access token by getAccessToken
+    if(isset($_GET['code']))
+    {
+        $res = webmasterApi::getAccessToken($_GET['code'], $client_id, $client_secret);
+
+        if (isset($res->access_token)) {
+            $_SESSION['access_token'] = $res->access_token;
+        } else die('bad code, try again');
+    }
+    // Else: redirect to oauth server
+    else
     {
         webmaster_api_example_tpl::redirect("https://oauth.yandex.ru/authorize?response_type=code&client_id=" . $client_id);
     }
 
-    $res = webmasterApi::getAccessToken($_GET['code'],$client_id,$client_secret);
-
-    if(isset($res->access_token))
-    {
-        $_SESSION['access_token'] = $res->access_token;
-    } else die('bad code, try again');
 }
 
 
