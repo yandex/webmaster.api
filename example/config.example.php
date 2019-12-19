b25ed59e1fe2cf7b0877f5f537f8fa30fc379fc3
Nikita Shabashov <niksh81@gmail.com>
2019-12-19 15:33:22 +0300

    close-api

--- example/config.example.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$path = realpath(dirname(__FILE__));
-$apiScript = realpath($path . "/../src/webmasterApi.php");
-
-// Get this code on https://oauth.yandex.com/client/new page
-$client_id = '';
-$client_secret = '';
